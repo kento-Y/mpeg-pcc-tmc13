80dc69f13c7e6ffc996c61c32635b98481e53724
kento-Y <kento_yamamoto@ruri.waseda.jp>
2021-09-22 23:32:47 +0900

    fix CMakeLists.txt

--- tmc3/TMC3.h
+++ tmc3/TMC3.h
@@ -38,17 +38,180 @@
 
 #define _CRT_SECURE_NO_WARNINGS
 
+#include <string>
 #include "TMC3Config.h"
 
 #include "pcc_chrono.h"
 
-struct Parameters;
+#include "PCCTMC3Encoder.h"
+#include "PCCTMC3Decoder.h"
+#include "ply.h"
+#include "frame.h"
+#include "hls.h"
+#include "PCCPointSet.h"
+#include "PCCMath.h"
 
-typedef pcc::chrono::Stopwatch<pcc::chrono::utime_inc_children_clock>
-  Stopwatch;
 
-bool ParseParameters(int argc, char* argv[], Parameters& params);
-int Compress(Parameters& params, Stopwatch&);
-int Decompress(Parameters& params, Stopwatch&);
+typedef pcc::chrono::Stopwatch<pcc::chrono::utime_inc_children_clock> Stopwatch;
+
+namespace pcc {
+
+enum class OutputSystem
+{
+  // Output after global scaling, don't convert to external system
+  kConformance = 0,
+
+  // Scale output to external coordinate system
+  kExternal = 1,
+};
+
+//----------------------------------------------------------------------------
+
+struct Parameters {
+  bool isDecoder;
+
+  // command line parsing should adjust dist2 values according to PQS
+  bool positionQuantizationScaleAdjustsDist2;
+
+  // Scale factor to apply when loading the ply before integer conversion.
+  // Eg, If source point positions are in fractional metres converting to
+  // millimetres will allow some fidelity to be preserved.
+  double inputScale;
+
+  // Length of the output point clouds unit vectors.
+  double outputUnitLength;
+
+  // output mode for ply writing (binary or ascii)
+  bool outputBinaryPly;
+
+  // Fractional fixed-point bits retained in conformance output
+  int outputFpBits;
+
+  // Output coordinate system to use
+  OutputSystem outputSystem;
+
+  // when true, configure the encoder as if no attributes are specified
+  bool disableAttributeCoding;
+
+  // Frame number of first file in input sequence.
+  int firstFrameNum;
+
+  // Number of frames to process.
+  int frameCount;
+
+  std::string uncompressedDataPath;
+  std::string compressedStreamPath;
+  std::string reconstructedDataPath;
+
+  // Filename for saving recoloured point cloud (encoder).
+  std::string postRecolorPath;
+
+  // Filename for saving pre inverse scaled point cloud (decoder).
+  std::string preInvScalePath;
+
+  EncoderParams encoder;
+  DecoderParams decoder;
+
+  // perform attribute colourspace conversion on ply input/output.
+  bool convertColourspace;
+
+  // resort the input points by azimuth angle
+  bool sortInputByAzimuth;
+};
+
+class SequenceCodec {
+public:
+  // NB: params must outlive the lifetime of the decoder.
+  SequenceCodec(Parameters* params) : params(params) {}
+
+  // Perform conversions and write output point cloud
+  //  \params cloud  a mutable copy of reconFrame.cloud
+  void writeOutputFrame(
+    const std::string& postInvScalePath,
+    const std::string& preInvScalePath,
+    const CloudFrame& reconFrame,
+    PCCPointSet3& cloud);
+
+  // determine the output ply scale factor
+  double outputScale(const CloudFrame& cloud) const;
+
+  // the output ply origin, scaled according to output coordinate system
+  Vec3<double> outputOrigin(const CloudFrame& cloud) const;
+
+  void scaleAttributesForInput(
+    const std::vector<AttributeDescription>& attrDescs, PCCPointSet3& cloud);
+
+  void scaleAttributesForOutput(
+    const std::vector<AttributeDescription>& attrDescs, PCCPointSet3& cloud);
+
+protected:
+  Parameters* params;
+};
+
+//----------------------------------------------------------------------------
+
+class SequenceEncoder
+  : public SequenceCodec
+  , PCCTMC3Encoder3::Callbacks {
+public:
+  // NB: params must outlive the lifetime of the decoder.
+  SequenceEncoder(Parameters* params);
+
+  int compress(Stopwatch* clock);
+
+protected:
+  int compressOneFrame(Stopwatch* clock);
+
+  void onOutputBuffer(const PayloadBuffer& buf) override;
+  void onPostRecolour(const PCCPointSet3& cloud) override;
+
+private:
+  ply::PropertyNameMap _plyAttrNames;
+
+  // The raw origin used for input sorting
+  Vec3<int> _angularOrigin;
+
+  PCCTMC3Encoder3 encoder;
+
+  std::ofstream bytestreamFile;
+
+  int frameNum;
+};
+
+//----------------------------------------------------------------------------
+
+class SequenceDecoder
+  : public SequenceCodec
+  , PCCTMC3Decoder3::Callbacks {
+public:
+  // NB: params must outlive the lifetime of the decoder.
+  SequenceDecoder(Parameters* params);
+
+  int decompress(Stopwatch* clock);
+
+protected:
+  void onOutputCloud(const CloudFrame& cloud) override;
+
+private:
+  PCCTMC3Decoder3 decoder;
+
+  std::ofstream bytestreamFile;
+
+  Stopwatch* clock;
+};
+
+}  // namespace pcc
+
+
+void convertToGbr(
+  const std::vector<pcc::AttributeDescription>& attrDescs, pcc::PCCPointSet3& cloud);
+
+void convertFromGbr(
+  const std::vector<pcc::AttributeDescription>& attrDescs, pcc::PCCPointSet3& cloud);
+
+//============================================================================
+bool ParseParameters(int argc, char* argv[], pcc::Parameters& params);
+// int Compress(Parameters& params, Stopwatch&);
+// int Decompress(Parameters& params, Stopwatch&);
 
 #endif /* TMC3_h */
